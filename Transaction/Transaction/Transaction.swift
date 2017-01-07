2b4079d9a76c252de224b69d06cab5350d3257a6
Bruno da Luz <brunolabx@gmail.com>
2017-01-07 12:31:01 -0200

    updated syntax

--- Transaction/Transaction/Transaction.swift
+++ Transaction/Transaction/Transaction.swift
@@ -1,48 +1,73 @@
 import Foundation
 
-open class Transaction {
+protocol Presentable {
+    func startOn(current: UIViewController,
+                 with: UIStoryboardSegue,
+                 options: UIViewAnimationOptions)
+    
+    func swapFrom(viewController: UIViewController,
+                  fromViewController: UIViewController,
+                  toViewController: UIViewController,
+                  options: UIViewAnimationOptions)
+    
+    func sizeFrom(viewController: UIViewController) -> CGSize
+}
+
+open class Transaction : Presentable {
     
     public init() {}
     
-    open func startOn(_ currentViewController: UIViewController,
-                        withSegue: UIStoryboardSegue,
-                        options: UIViewAnimationOptions) {
+    open func startOn(current: UIViewController,
+                      with withSegue: UIStoryboardSegue,
+                      options: UIViewAnimationOptions) {
         
-        if currentViewController.childViewControllers.count > 0 {
-            swapFromViewController(currentViewController,
-                                   fromViewController: currentViewController.childViewControllers.last!,
-                                   toViewController: withSegue.destination,
-                                   options: options)
-        } else {
-            currentViewController.addChildViewController(withSegue.destination)
-            withSegue.destination.view.frame = CGRect(x: 0, y: 0,
-                                                                        width: currentViewController.view.frame.size.width,
-                                                                        height: currentViewController.view.frame.size.height)
-            currentViewController.view.addSubview(withSegue.destination.view)
-            withSegue.destination.didMove(toParentViewController: currentViewController)
+        guard current.childViewControllers.count > 0 else {
+            swapFrom(viewController: current,
+                     fromViewController: current.childViewControllers.last!,
+                     toViewController: withSegue.destination,
+                     options: options)
+            return
+        }
+        
+        let size = sizeFrom(viewController: current)
+        
+        current.addChildViewController(withSegue.destination)
+        withSegue.destination.view.frame = CGRect(x: 0,
+                                                  y: 0,
+                                                  width: size.width,
+                                                  height: size.height)
+        
+        current.view.addSubview(withSegue.destination.view)
+        withSegue.destination.didMove(toParentViewController: current)
+    }
+    
+    internal func swapFrom(viewController: UIViewController,
+                           fromViewController: UIViewController,
+                           toViewController: UIViewController,
+                           options: UIViewAnimationOptions) {
+        
+        let size = sizeFrom(viewController: viewController)
+        
+        toViewController.view.frame = CGRect(x: 0,
+                                             y: 0,
+                                             width: size.width,
+                                             height: size.height)
+        
+        fromViewController.willMove(toParentViewController: nil)
+        viewController.addChildViewController(toViewController)
+        
+        viewController.transition(from: fromViewController,
+                                  to: toViewController,
+                                  duration: 0.4,
+                                  options: options,
+                                  animations: nil) { finished in
+                                    fromViewController.removeFromParentViewController()
+                                    toViewController.didMove(toParentViewController: viewController)
         }
     }
     
-    fileprivate func swapFromViewController(_ currentViewController: UIViewController,
-                                        fromViewController: UIViewController,
-                                        toViewController: UIViewController,
-                                        options: UIViewAnimationOptions) {
-        
-        toViewController.view.frame = CGRect(x: 0, y: 0,
-                                                 width: currentViewController.view.frame.size.width,
-                                                 height: currentViewController.view.frame.size.height)
-        
-        fromViewController.willMove(toParentViewController: nil)
-        currentViewController.addChildViewController(toViewController)
-        
-        currentViewController.transition(from: fromViewController,
-                                                           to: toViewController,
-                                                           duration: 0.4,
-                                                           options: options,
-                                                           animations: nil) { finished in
-                                                            fromViewController.removeFromParentViewController()
-                                                            toViewController.didMove(toParentViewController: currentViewController)
-        }
+    func sizeFrom(viewController: UIViewController) -> CGSize {
+        return viewController.view.frame.size
     }
     
 }
