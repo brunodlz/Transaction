5d6da036ebe37f06ee1ef810bbc044c821144218
Bruno da Luz <brunolabx@gmail.com>
2017-01-07 15:33:32 -0200

    configuring controller flow

--- ExampleWithStoryboard/ExampleWithStoryboard/ViewController.swift
+++ ExampleWithStoryboard/ExampleWithStoryboard/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import Transaction
+
+enum Segue: String {
+    case First
+    case Second
+}
 
 class ViewController: UIViewController {
 
+    let animation = Transaction()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        showFirstSegue()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Segue.First.rawValue {
+            animation.startOn(current: self, with: segue, options: .transitionFlipFromRight)
+        } else if segue.identifier == Segue.Second.rawValue {
+            animation.startOn(current: self, with: segue, options: .transitionFlipFromLeft)
+        }
+    }
+    
+    func showFirstSegue() {
+        performSegue(withIdentifier: Segue.First.rawValue, sender: nil)
+    }
+    
+    func showSecondSegue() {
+        performSegue(withIdentifier: Segue.Second.rawValue, sender: nil)
+    }
 }
 
