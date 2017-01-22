58dfdc7acfdd331a732b9faebb3be3a07b678a88
Bruno da Luz <brunolabx@gmail.com>
2017-01-22 00:33:06 -0200

    deleted storyboard and adding navigation view controller

--- ExampleWithViewCode/ExampleWithViewCode/ExampleWithViewCode/AppDelegate.swift
+++ ExampleWithViewCode/ExampleWithViewCode/ExampleWithViewCode/AppDelegate.swift
@@ -15,7 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = ViewController()
+        
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.isNavigationBarHidden = true
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
